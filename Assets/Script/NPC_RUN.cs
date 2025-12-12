760500e5eeb00db1acabf3d1564029661037a1e0
pinxun93 <pinxunlai0327@gmail.com>
2025-12-12 11:09:42 +0800

    作業更新進度 (12/12)

--- Assets/Script/NPC_RUN.cs
+++ Assets/Script/NPC_RUN.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 using TMPro;
+using UnityEditor;
 
 public class NPC_RUN : MonoBehaviour
 {
@@ -11,12 +12,18 @@ public class NPC_RUN : MonoBehaviour
 
     public TextMeshPro 血量文字;
     public int 血量 = 100;
+    int 原始血量;
     public Transform 血條;
+    bool 開始攻擊 = false;
+    float 攻擊間距 = 2f;
+    float 下次可攻擊時間;
 
     void Start()
     {
         導航 = GetComponent<NavMeshAgent>();
         動畫 = GetComponent<Animator>(); // 對應 Animator
+        原始血量 = 血量;
+        血量文字.text = 血量.ToString();
     }
 
     void Update()
@@ -29,10 +36,21 @@ public class NPC_RUN : MonoBehaviour
             if (距離 <= 3.1f)
             {
                 動畫.SetBool("iswalk", false);
+                開始攻擊 = true;
             }
             else
             {
                 動畫.SetBool("iswalk", true);
+                開始攻擊 = false;
+            }
+            if (開始攻擊)
+            {
+                if (血量 <= 0) { return; }
+                if (Time.time >= 下次可攻擊時間)
+                {
+                    動畫.SetTrigger("isAttack");
+                    下次可攻擊時間 = Time.time + 攻擊間距;
+                }
             }
         }
     }
@@ -44,11 +62,16 @@ public class NPC_RUN : MonoBehaviour
             Destroy(other.gameObject);
             血量--;
             血量文字.text = 血量.ToString();
-            float 血量比例 = (float)血量/100f;
+            float 血量比例 = (float)血量/(float)原始血量;
             血條.localScale = new Vector3(血量比例,1,1);
-            if(血量 <= 0)
+            if (血量 <= 0)
             {
-                Destroy(this.gameObject);
+                Destroy(this.gameObject,3f);
+                動畫.SetTrigger("isDead");
+            }
+            else
+            {
+                動畫.SetTrigger("isHit");
             }
         }
     }
